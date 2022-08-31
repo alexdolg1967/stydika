f7778b6f82aa334d49f7b4a621c485415a55fff4
alexdolg1967 <alexdolg1967@mail.ru>
2022-08-31 06:37:57 +0300

    fix js

--- src/js/_vendor.js
+++ src/js/_vendor.js
@@ -1,5 +1,5 @@
 import "./vendor/focus-visible.js";
 import "./vendor/fslightbox.js";
-import "./vendor/inputmask.min.js";
-import "./vendor/dragscroll.js";
+// import "./vendor/inputmask.min.js";
+// import "./vendor/dragscroll.js";
 import "./vendor/dynamic_adapt.js";
